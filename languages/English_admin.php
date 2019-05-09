b5f10b392372ee2958275daa6043c3c088c4876c
Jimmi08 <janamicek@gmail.com>
2019-05-09 20:23:01 +0200

    language folder

---
+++ languages/English_admin.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2016 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * 
+ *    e107 website system - Language File.
+ */
+ 
+ 
+ 
+define("LAN_PLUGIN_VB_MODE_PROD", "Production");
+define("LAN_PLUGIN_VB_MODE_DEV", "Development");
+define("LAN_PLUGIN_VB_TR_CAPTION", "TableRender Caption"); 
+ 
